b3ed423bfb7abaed2fd54c24c4fc333ba8f16b3e
DmitriiPalmin <hypere@yandex.ru>
2021-09-22 19:41:46 +0300

    Попытка ФЧХ и АЧХ

--- Lab1.py
+++ Lab1.py
@@ -68,13 +68,16 @@ def getUnit(name):
 unit_name = choice()
 unit = getUnit(unit_name)
 
-def graph(num, title, y, x):
+def graph(num, title, y1, y2, x):
     pyplot.subplot(2,1, num)
     pyplot.grid(True)
     if title == 'Переходная характеристика':
-        pyplot.plot(x, y, 'purple')
+        pyplot.plot(x, y1, 'purple')
     elif title == 'Импульсная характеристика':
-        pyplot.plot(x, y, 'green')
+        pyplot.plot(x, y1, 'green')
+    elif title == 'Боде':
+        pyplot.plot(x, y1, 'blue')
+        pyplot.plot(x, y2, 'red')
     pyplot.title(title)
     pyplot.ylabel('Амплитуда')
     pyplot.xlabel('Время (с)')
@@ -87,9 +90,13 @@ for i in range(0, 10000):
     timeLine.append(i/1000)
 
 [y, x] = matlab.step(unit, timeLine)
-graph(1, 'Переходная характеристика', y, x )
+graph(1, 'Переходная характеристика', y,0, x )
 [y, x] = matlab.impulse(unit, timeLine)
 
-graph(2, 'Импульсная характеристика', y, x )
+graph(2, 'Импульсная характеристика', y, 0, x )
 
+mag, phase, omega = matlab.bode(unit, timeLine)
+
+graph(2, 'Боде', mag, phase, omega)
+# pyplot.plot()
 pyplot.show()
